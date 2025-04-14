c26360206b52a6d6f721ed2d7cc0af60b5e3e83f
AMS21 <AMS21.github@gmail.com>
2025-04-14 11:31:00 +0200

    feat: Remove thiserror dependency

--- src/error.rs
+++ src/error.rs
@@ -2,26 +2,61 @@
 
 use reqwest::StatusCode;
 
-#[derive(thiserror::Error, Debug)]
+#[derive(Debug)]
 pub enum Error {
-    #[error("Request failed: {0}")]
     RequestFailed(StatusCode),
-
-    #[error("Empty response")]
     EmptyResponse,
-
-    #[error(transparent)]
-    IO(#[from] std::io::Error),
-
-    #[error(transparent)]
-    Reqwest(#[from] reqwest::Error),
-
-    #[error(transparent)]
-    TracingDispatcherSetGlobalDefault(#[from] tracing::dispatcher::SetGlobalDefaultError),
-
-    #[error(transparent)]
-    RegexPattern(#[from] regex::Error),
-
-    #[error("Invalid URL: {0}")]
     InvalidUrl(String),
+
+    // -- External --
+    IO(std::io::Error),
+    Reqwest(reqwest::Error),
+    TracingDispatcherSetGlobalDefault(tracing::dispatcher::SetGlobalDefaultError),
+    RegexPattern(regex::Error),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::RequestFailed(status_code) => {
+                write!(f, "Request failed with status code: {status_code}")
+            }
+            Self::EmptyResponse => write!(f, "Received empty response"),
+            Self::InvalidUrl(url) => write!(f, "Invalid URL: {url}"),
+
+            // -- External --
+            Self::IO(err) => write!(f, "IO error: {err}"),
+            Self::Reqwest(err) => write!(f, "Reqwest error: {err}"),
+            Self::TracingDispatcherSetGlobalDefault(err) => {
+                write!(f, "Tracing dispatcher error: {err}")
+            }
+            Self::RegexPattern(err) => write!(f, "Regex pattern error: {err}"),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(value: std::io::Error) -> Self {
+        Self::IO(value)
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(value: reqwest::Error) -> Self {
+        Self::Reqwest(value)
+    }
+}
+
+impl From<tracing::dispatcher::SetGlobalDefaultError> for Error {
+    fn from(value: tracing::dispatcher::SetGlobalDefaultError) -> Self {
+        Self::TracingDispatcherSetGlobalDefault(value)
+    }
+}
+
+impl From<regex::Error> for Error {
+    fn from(value: regex::Error) -> Self {
+        Self::RegexPattern(value)
+    }
 }
